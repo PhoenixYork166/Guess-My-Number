456db877e642a17fa0dfc9eb14867fa8c005137d
phoenixyork166 <berlinlee2017@gmail.com>
2023-05-29 12:01:40 +0800

    fixing styling

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ guess.addEventListener('change', (event) => {
     console.log('guess value\n', event.target.value) 
 })
 
-
+// For 'Check!' button logic
 function returnGuess() {
     if (Number(guess.value) > 20 || Number(guess.value) < 0 || Number(guess.value) === 0 || guess.value.includes('.')) {
         message.textContent = 'Guess must be between 1 and 20!'
@@ -55,12 +55,16 @@ function returnGuess() {
         
     } 
     
+    // When score = 0 --> Losing game
     if (score_number === 0) {
-        // If score reaches 0, disable Check! button
+        // Telling player about losing game
         message.textContent = 'Game Over! You lost'
+        // Telling player about losing game
         number.textContent = ':('
+        // If score reaches 0, disable Check! button
         btn_check.disabled = true
     } else if (score_number < 1) {
+        // Never allow score going below 0
         btn_check.disabled = true
     }
     
@@ -68,16 +72,25 @@ function returnGuess() {
 
 btn_check.addEventListener('click', returnGuess)
 
+// button 'Again!' logic
 function resetGame() {
     // Re-randomize number.value on next round
     number.value = randomFromInterval(1, 20)
     message.textContent = 'Start guessing...'
-    body.style.background = "#222"
+    // Change back body.style.backgroundColor to black
+    body.style.backgroundColor = "#222"
+    // Emptying guess input
     guess.value = ''
+    // Declaring score.textContent = 20
     score.textContent = Number('20').toString()
+    // Storing score in our code
     score_number = Number(score.textContent)
+    // Reactivate 'Check!' button
     btn_check.disabled = false
+    // Change Secret number back to ?
     number.textContent = '?'
+    // Change Secret number width back to normal
+    number.style.width = '15rem'
     // Logging
     console.log('message.textContent\n', message.textContent)
     console.log('guess.value\n', guess.value)
